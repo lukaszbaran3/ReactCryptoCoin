adb01e81293ab8b4151d2b82ad2c07c923ed4c90
lukaszbaran3 <lukasz.baran0330@gmail.com>
2023-06-28 23:45:18 +0200

    deleted @mui

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,25 +1,23 @@
 import React, { useState } from "react";
-import { useMediaQuery } from "@material-ui/core";
 import "../css/DropdownMenu.css";
 
 function DropdownMenu() {
-  const isScreenSmall = useMediaQuery("(max-width: 600px)");
   const [showMenu, setShowMenu] = useState(false);
 
   const handleClick = () => {
     setShowMenu(!showMenu);
   };
+
   return (
     <div>
-      {isScreenSmall && (
-        <button className="open-menu" onClick={handleClick}>
-          <i className="fa-solid fa-bars"></i>
-        </button>
-      )}
-      {showMenu ? <SmallNavbar /> : null}
+      <button className="open-menu" onClick={handleClick}>
+        <i className="fa-solid fa-bars"></i>
+      </button>
+      {showMenu && <SmallNavbar />}
     </div>
   );
 }
+
 const SmallNavbar = () => {
   return (
     <div>
